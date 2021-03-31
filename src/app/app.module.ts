fdcf5e1572254d1e653921468ad835034b1bc61f
Fastus <fastus6666@gamil.com>
2021-03-31 15:26:24 +0200

    Added routing to the project. Closes #1.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,18 +8,29 @@ import { NavComponent } from './components/nav/nav.component';
 import {MatButtonModule} from '@angular/material/button';
 import { MapComponent } from './components/map/map.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BadrequestComponent } from './components/badrequest/badrequest.component';
+
+const appRoutes: Routes = [
+  {path: 'map', component: MapComponent},
+  {path: '', redirectTo: '/map', pathMatch: 'full'},
+  {path: '**', component: BadrequestComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    MapComponent
+    MapComponent,
+    BadrequestComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes
+    ),
   ],
   providers: [],
   bootstrap: [AppComponent]
